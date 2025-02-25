520233e28a54e69506412f92969b6ff980f02db1
Abdón Hernández <102083933+abdoonhdz@users.noreply.github.com>
2025-02-25 15:13:16 +0100

    Revert "Tsk card detail"

--- src/app/shared/cards/card-detail/card-detail.component.ts
+++ src/app/shared/cards/card-detail/card-detail.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
 
-import { MaterialModule } from '../../../material/material.module';
-
-export const VEHICLE_MOCK = {
-  id: 1,
-  license: '8631GCH',
-  brand: 'Toyota',
-  model: 'Corolla',
-  color: 'Rojo',
-  type: 'Coche',
-  doors: '5'
-};
-
 @Component({
   selector: 'app-card-detail',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [],
   templateUrl: './card-detail.component.html',
   styleUrl: './card-detail.component.scss'
 })
 export class CardDetailComponent {
-
-  vehicle: any;
-
-  ngOnInit(): void {
-    this.vehicle = VEHICLE_MOCK;
-  }
 
 }
