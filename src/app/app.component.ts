3a5fd95e160edc9935df2521b76fdcb802c58d02
Rodrigo Gonzalez Dominguez <rodrigo.gonzalezdomi@plexus.tech>
2025-02-25 13:59:34 +0100

    card view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardViewComponent } from "./shared/cards/card-view/card-view.component";
+import { CardDetailComponent } from './shared/cards/card-detail/card-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardViewComponent],
+  imports: [RouterOutlet, CardDetailComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontendAngular';
