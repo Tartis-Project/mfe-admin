09858c4f33949edb5270a1ba1a6699ea5b49eb07
Hugespain <diegosilva3795@gmail.com>
2025-02-25 15:18:47 +0100

    Navbarv0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardViewComponent } from "./shared/cards/card-view/card-view.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardViewComponent],
+  imports: [RouterOutlet, CardViewComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
