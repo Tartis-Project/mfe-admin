04a0241f5a86a0f6490953015287a3b4d7451af5
Abdón Hernández <abdonhc70@gmail.com>
2025-02-26 08:44:08 +0100

    Diseño de la card aplicado a todos sus usos.
    Otros:
    - Fixeado ancho del main.

--- src/app/pages/vehicles/vehicle-list/vehicle-list.component.ts
+++ src/app/pages/vehicles/vehicle-list/vehicle-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardViewComponent } from "../../../shared/cards/card-view/card-view.component";
 
 @Component({
   selector: 'app-vehicle-list',
   standalone: true,
-  imports: [],
+  imports: [CardViewComponent],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
 })
