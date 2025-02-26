1bb79ee60238957fcbbd082b3165465e132a99d7
Rodrigo Gonzalez Dominguez <rodrigo.gonzalezdomi@plexus.tech>
2025-02-26 08:53:00 +0100

    vehicle detail only visual

--- src/app/pages/vehicles/vehicle-detail/vehicle-detail.component.ts
+++ src/app/pages/vehicles/vehicle-detail/vehicle-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardDetailComponent } from '../../../shared/cards/card-detail/card-detail.component';
+import { MaterialModule } from '../../../material/material.module';
 
 @Component({
   selector: 'app-vehicle-detail',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CardDetailComponent],
   templateUrl: './vehicle-detail.component.html',
   styleUrl: './vehicle-detail.component.scss'
 })
