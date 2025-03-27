9a35f3cb4e1eee2efcf51e63dc45c8a530d8fd9f
Abdón Hernández <abdonhc70@gmail.com>
2025-03-27 21:13:35 +0100

    Puerto actualizado para Keycloak + componentes adaptados

--- src/app/pages/admin/pages/admin-list/admin-list.component.ts
+++ src/app/pages/admin/pages/admin-list/admin-list.component.ts
@@ -1,39 +1,39 @@
-// import { Component } from '@angular/core';
-// import { KeycloakService } from 'keycloak-angular';
+import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
-// import { AdminService } from '../../services/administrator.service';
-// import { MaterialModule } from '../../../../material/material.module';
-// import { Administrator } from '../../interfaces/administrator.model';
+import { AdminService } from '../../services/administrator.service';
+import { MaterialModule } from '../../../../material/material.module';
+import { Administrator } from '../../interfaces/administrator.model';
 
-// @Component({
-//   selector: 'app-admin-list',
-//   standalone: true,
-//   imports: [MaterialModule],
-//   templateUrl: './admin-list.component.html',
-//   styleUrl: './admin-list.component.scss',
-// })
-// export class AdminListComponent {
-//   administrators: Administrator[] = [];
-//   displayedColumns: string[] = ['firstName', 'lastName', 'email', 'username'];
+@Component({
+  selector: 'app-admin-list',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './admin-list.component.html',
+  styleUrl: './admin-list.component.scss',
+})
+export class AdminListComponent {
+  administrators: Administrator[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'username'];
 
-//   constructor(
-//     private adminService: AdminService,
-//     private keycloakService: KeycloakService,
-//   ) { }
+  constructor(
+    private adminService: AdminService,
+    private keycloakService: KeycloakService,
+  ) { }
 
-//   ngOnInit(): void {
-//     this.loadUsers();
-//   }
+  ngOnInit(): void {
+    this.loadUsers();
+  }
 
-//   loadUsers(): void {
-//     this.adminService.getAllUsers().subscribe(
-//       (users) => {
-//         this.administrators = users;
-//         console.log('Usuarios cargados:', this.administrators);
-//       },
-//       (error) => {
-//         console.error('Error obteniendo usuarios:', error);
-//       },
-//     );
-//   }
-// }
+  loadUsers(): void {
+    this.adminService.getAllUsers().subscribe(
+      (users) => {
+        this.administrators = users;
+        console.log('Usuarios cargados:', this.administrators);
+      },
+      (error) => {
+        console.error('Error obteniendo usuarios:', error);
+      },
+    );
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://34.175.249.11:8000/management',
+  apiUrl: 'https://34.175.249.11:443/management',
 };
